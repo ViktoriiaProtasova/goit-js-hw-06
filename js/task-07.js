dedf5e96ac9cd4aba99eb4dd5b8c6af800857ff0
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-04-22 23:37:07 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,10 @@
 const inputEl = document.querySelector("#font-size-control");
 const outputText = document.querySelector("#text");
 
+outputText.style.fontSize = `${
+  (Number(inputEl["max"]) + Number(inputEl["min"])) / 2
+}px`;
+
 const handleInput = () => {
   outputText.style.fontSize = `${inputEl.value}px`;
 };
